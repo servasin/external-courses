9b28c0b20c844940e16497fe10f9f747fec49eb5
Sergey Vasin <vvs221@bk.ru>
2020-03-21 16:31:58 +0300

    fix(ex5__js): fixed return

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -6,24 +6,28 @@ function Calculator() {
     if (typeof num === 'number' && !Number.isNaN(num)) {
       count += num;
     }
+
     return calculator.add;
   };
   this.subtract = function(num) {
     if (typeof num === 'number' && !Number.isNaN(num)) {
       count -= num;
     }
+
     return calculator.subtract;
   };
   this.divide = function(num) {
     if (typeof num === 'number' && !Number.isNaN(num)) {
       count /= num;
     }
+
     return calculator.divide;
   };
   this.multiply = function(num) {
     if (typeof num === 'number' && !Number.isNaN(num)) {
       count *= num;
     }
+
     return calculator.multiply;
   };
   this.getResult = function() {
