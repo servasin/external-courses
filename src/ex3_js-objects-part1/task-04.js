3e11063467c938ea6eb9129231f5d649e85d125a
Sergey Vasin <vvs221@bk.ru>
2020-03-24 22:09:38 +0300

    fix(ex3__js): added Object.assing for task 4

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,21 +1,18 @@
 'use strict'
 
 function addNewProp(str, obj) {
-  if (typeof str === 'string' &&
-      typeof obj === 'object' && !Array.isArray(obj)) {
+  if (typeof str === 'string'
+      && typeof obj === 'object'
+      && !Array.isArray(obj)) {
 
-        if ( !(str in obj) ) {
-          obj[str] = 'new'// eslint-disable-line no-param-reassign
-          return obj;
+        let newObj = Object.assign
+        if ( !(str in newObj) ) {
+          newObj[str] = 'new'
+
+          return newObj;
         }
   }
   return obj;
 }
-
-// Если не отключить линт на строке, то тесты не проходятся, т.к.
-// проверяется  исходный объект, поэтому не получается использовать Object.assing
-
-// https://drive.google.com/file/d/1mOsHUl4lEVwkGDL7xYD8yuExXlSM_ASl/view?usp=sharing
-// https://drive.google.com/file/d/1inEg8aEl0R-3sPFBH29zNNfftyLw4HXY/view?usp=sharing
 
 module.exports = addNewProp;
