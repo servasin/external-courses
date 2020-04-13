60d705482d2c096955cff15640ea3cafdff42dc8
Sergey Vasin <vvs221@bk.ru>
2020-04-13 22:11:46 +0300

    (fix/ kanban) change let to const, rename toggle to showMenu

--- src/kanban/js/script.js
+++ src/kanban/js/script.js
@@ -1,27 +1,27 @@
 'use strict'
 
-let btn = document.querySelector('.header__profile-btn');
-let toggle = true;
-let parentNode = document.querySelector('.header__profile');
-let div = document.createElement('div');
+const btn = document.querySelector('.header__profile-btn');
+const parentNode = document.querySelector('.header__profile');
+const div = document.createElement('div');
 div.className = 'header__profile-info';
 div.innerHTML =  
 `<p class="header__profile-name">Sergey Vasin</p>
 <p class="header__profile-position">Frontend Developer</p>
 <button class="header__profile-login">login</button>`;
+let showMenu = false;
 
 function addMenu () {
-	if(!toggle) {
+	if(showMenu) {
 		div.remove();
 		btn.classList.remove('header__profile-btn--rotate')
-		toggle = true;
+		showMenu = false;
 		
 		return;
 	}
 
 	parentNode.after(div);
 	btn.classList.add('header__profile-btn--rotate')
-	toggle = false;
+	showMenu = true;
 
 	return;
 }
